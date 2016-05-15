5060ec14c4dd998488955b53e9cf1d1d6a3b26c7
Sani Yusuf <saniyusuf@gmail.com>
2016-05-15 19:51:32 +0100

    refactored project service

--- www/js/services/project.service.js
+++ www/js/services/project.service.js
@@ -6,16 +6,16 @@ angular
     .module('starter.services')
     .factory('ProjectService', ProjectService);
 
-ProjectService.$inject = ['$q', '_', 'devUtils', 'logger'];
+ProjectService.$inject = ['$q', 'devUtils'];
 
-function ProjectService($q, _, devUtils, logger) {
+function ProjectService($q, devUtils) {
     var getProjectsFromSmartStorePromise = $q.defer(), getProjectFromSmartSqlPromise = $q.defer(),
         getProjectSqlQuery,
         projectsTableName = 'MC_Project__ap';
 
     var projectService = {
         getAllProjects: getAllProjects,
-        getProject: getProject
+        getProjectDetail: getProjectDetail
     };
 
     return projectService;
@@ -23,29 +23,26 @@ function ProjectService($q, _, devUtils, logger) {
     function getAllProjects() {
         return devUtils.readRecords(projectsTableName, [])
             .then(function (projectsRecordsSuccessResponse) {
-                logger.log('Successfully Got All Projects From Smart Store: -> ', projectsRecordsSuccessResponse.records);
                 getProjectsFromSmartStorePromise.resolve(projectsRecordsSuccessResponse.records);
                 return getProjectsFromSmartStorePromise.promise;
 
             }, function (projectsRecordsFailureResponse) {
-                logger.log('Failed To Get All Projects From Smart Store: Reason -> ', projectsRecordsFailureResponse);
                 getProjectsFromSmartStorePromise.reject(projectsRecordsFailureResponse);
                 return getProjectsFromSmartStorePromise.promise;
             });
     }
 
-    function getProject(projectID) {
+    function getProjectDetail(projectID) {
         getProjectSqlQuery =
-            'SELECT * FROM {' + projectsTableName + '} as project ' +
-            'WHERE project.{' + projectsTableName +':Id} = ' + projectID + ';';
+            "SELECT * FROM {" + projectsTableName + "} " +
+            "WHERE {" + projectsTableName + ":Id} = '" + projectID + "';";
+        console.log('Query -> ', getProjectSqlQuery);
         return devUtils.smartSql(getProjectSqlQuery)
             .then(function (projectSuccessResponse) {
-                logger.log('Successfully Got Project With Smart Sql: ', projectSuccessResponse.records);
                 getProjectFromSmartSqlPromise.resolve(projectSuccessResponse.records);
                 return getProjectFromSmartSqlPromise.promise;
 
             }, function (projectFailureResponse) {
-                logger.log('Failed To Get Project With Smart SQL: Reason -> ', projectFailureResponse);
                 getProjectFromSmartSqlPromise.reject(projectFailureResponse);
                 return getProjectFromSmartSqlPromise.promise;
             });
