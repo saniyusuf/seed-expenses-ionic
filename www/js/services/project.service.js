5514b8e2bb2ef9b0ece25f82d61b1610d8c3145a
Sani Yusuf <saniyusuf@gmail.com>
2016-05-31 13:57:57 +0100

    fixed project sumamry and details merging

--- www/js/services/project.service.js
+++ www/js/services/project.service.js
@@ -6,16 +6,19 @@ angular
     .module('starter.services')
     .factory('ProjectService', ProjectService);
 
-ProjectService.$inject = ['$q', 'devUtils'];
+ProjectService.$inject = ['$q', 'devUtils', '_', 'logger'];
 
-function ProjectService($q, devUtils) {
-    var getProjectsFromSmartStorePromise = $q.defer(), getProjectFromSmartSqlPromise = $q.defer(),
-        getProjectSqlQuery,
-        projectsTableName = 'MC_Project__ap';
+function ProjectService($q, devUtils, _, logger) {
+    var getProjectsFromSmartStorePromise = $q.defer(),
+        getProjectFromSmartSqlPromise = $q.defer(),
+        getProjectSummaryPromise = $q.defer(),
+        getProjectSqlQuery, getProjectExpensesQuery,
+        projectsTableName = 'MC_Project__ap', projectExpensesTableName = "MC_Time_Expense__ap";
 
     var projectService = {
         getAllProjects: getAllProjects,
-        getProjectDetail: getProjectDetail
+        getProjectDetail: getProjectDetail,
+        getProjectSummary: getProjectSummary
     };
 
     return projectService;
@@ -32,20 +35,81 @@ function ProjectService($q, devUtils) {
             });
     }
 
+    function getProjectSummary(projectID) {
+        var projectTimeTotal = 0;
+        var projectExpensesTotal = 0;
+        var timeAndExpenseProjects;
+
+        getProjectExpensesQuery =
+            "SELECT * FROM {" + projectExpensesTableName + "} " +
+            "WHERE {" + projectExpensesTableName + ":mobilecaddy1__Project__c} = '" + projectID + "';";
+        logger.log('Get Time & Expenses Total Smart SQL Query -> ', getProjectSqlQuery);
+
+        return devUtils.smartSql(getProjectExpensesQuery)
+            .then(function (timeAndExpenseProjectsSuccessResponse) {
+                if(!timeAndExpenseProjectsSuccessResponse.records.length || timeAndExpenseProjectsSuccessResponse.records.length < 1){
+                    getProjectSummaryPromise.resolve({
+                        projectTimeTotal: projectTimeTotal,
+                        projectExpensesTotal: projectExpensesTotal
+                    });
+
+                    return getProjectSummaryPromise.promise;
+                }
+
+                timeAndExpenseProjects = _.where(
+                    timeAndExpenseProjectsSuccessResponse.records,
+                    {'mobilecaddy1__Project__c': projectId}
+                );
+
+                angular.forEach(function (timeAndExpenseProject) {
+                    if (!isNullOrUndefined(timeAndExpenseProject.mobilecaddy1__Duration_Minutes__c)){
+                        projectTimeTotal += timeAndExpenseProject.mobilecaddy1__Duration_Minutes__c;
+                    }
+
+                    if (!isNullOrUndefined(timeAndExpenseProject.mobilecaddy1__Expense_Amount__c)){
+                        projectExpensesTotal += timeAndExpenseProject.mobilecaddy1__Expense_Amount__c;
+                    }
+                });
+
+                getProjectSummaryPromise.resolve({
+                    projectTimeTotal: projectTimeTotal,
+                    projectExpensesTotal: projectExpensesTotal
+                });
+                return getProjectSummaryPromise.promise;
+
+            }, function (timeAndExpenseProjectsFailureResponse) {
+                getProjectSummaryPromise.reject(timeAndExpenseProjectsFailureResponse);
+                return getProjectSummaryPromise.promise;
+            });
+
+    }
+
     function getProjectDetail(projectID) {
         getProjectSqlQuery =
             "SELECT * FROM {" + projectsTableName + "} " +
             "WHERE {" + projectsTableName + ":Id} = '" + projectID + "';";
-        console.log('Query -> ', getProjectSqlQuery);
+        logger.log('Get Projects Smart SQL Query -> ', getProjectSqlQuery);
         return devUtils.smartSql(getProjectSqlQuery)
             .then(function (projectSuccessResponse) {
-                getProjectFromSmartSqlPromise.resolve(projectSuccessResponse.records);
-                return getProjectFromSmartSqlPromise.promise;
+                return getProjectSummary(projectID)
+                    .then(function (projectSummary) {
+                        projectSuccessResponse.records[0].projectTotals = projectSummary;
+                        getProjectFromSmartSqlPromise.resolve(projectSuccessResponse.records[0]);
+                        return getProjectFromSmartSqlPromise.promise;
+
+                    }, function (projectSummaryFailureResponse) {
+                        getProjectFromSmartSqlPromise.reject(projectSummaryFailureResponse);
+                        return getProjectFromSmartSqlPromise.promise;
+                    });
 
             }, function (projectFailureResponse) {
                 getProjectFromSmartSqlPromise.reject(projectFailureResponse);
                 return getProjectFromSmartSqlPromise.promise;
             });
+    }
+
+    function isNullOrUndefined(variableToBeChecked) {
+        return variableToBeChecked === null || typeof variableToBeChecked === 'undefined';
     }
 
 }
