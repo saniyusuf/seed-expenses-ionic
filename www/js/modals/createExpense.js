0e3e20a20b935cf8881ed796d11de19c684973c0
Sani Yusuf <saniyusuf@gmail.com>
2016-06-02 12:10:02 +0100

    inital commit

---
+++ www/js/modals/createExpense.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Sani Yusuf on 02/06/2016.
+ */
+
+(function () {
+    'use strict';
+    angular
+        .module('starter.controllers')
+        .factory('CreateExpenseModal', CreateExpenseModal);
+
+    function CreateExpenseModal() {
+
+    }
+
+
+})();
