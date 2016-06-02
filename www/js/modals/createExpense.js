97ca41a3754066aea8e96a9f2f2f8e066e2d127e
Sani Yusuf <saniyusuf@gmail.com>
2016-06-02 13:15:12 +0100

    created modal with open close methods

--- www/js/modals/createExpense.js
+++ www/js/modals/createExpense.js
@@ -8,9 +8,52 @@
         .module('starter.controllers')
         .factory('CreateExpenseModal', CreateExpenseModal);
 
-    function CreateExpenseModal() {
+    CreateExpenseModal.$inject = ['$ionicModal', '$rootScope'];
+
+    function CreateExpenseModal($ionicModal, $rootScope) {
+        var $scope = $rootScope.$new(),
+            createExpenseModalInstance = {},
+            createExpenseModalInstanceOptions = {
+                scope: $scope,
+                focusFirstInput: true
+            },
+            createExpenseModalTemplateUrl = RESOURCE_ROOT + 'templates/modals/createExpense.html';
+
+        var createExpenseModal = {
+            open: open,
+            close: close
+        };
+
+        return createExpenseModal;
+
+        function open() {
+            $scope = {
+                expenseDescription: '',
+                expenseAmount: '',
+                saveExpense: saveExpense
+            };
+
+            return $ionicModal.fromTemplateUrl(
+                createExpenseModalTemplateUrl,
+                createExpenseModalInstanceOptions
+
+            ).then(function (modalInstance) {
+                    createExpenseModal.close = close(modalInstance);
+                    return modalInstance.show();
+                });
+        }
+
+        function close(modalInstance) {
+            return modalInstance.hide()
+                .then(function (modalToBeRemoved) {
+                    return modalToBeRemoved.remove();
+                });
+        }
+
+        function saveExpense() {
+
+        }
 
     }
-
 
 })();
