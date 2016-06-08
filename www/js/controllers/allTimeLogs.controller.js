f621b689a27ac9662db9c4ff0ac895a8921c9390
Sani Yusuf <saniyusuf@gmail.com>
2016-06-08 23:27:28 +0100

    added time logs controller

---
+++ www/js/controllers/allTimeLogs.controller.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Sani on 08/06/2016.
+ */
+
+
+(function () {
+    'use strict';
+
+    angular
+        .module('starter.controllers')
+        .controller('AllTimeLogsController', AllTimeLogsController);
+
+    AllTimeLogsController.$inject = ['AllTimeLogs'];
+
+    function AllTimeLogsController(AllTimeLogs) {
+        var vm = this;
+
+        vm.allExpenses = AllTimeLogs;
+    }
+
+})();
