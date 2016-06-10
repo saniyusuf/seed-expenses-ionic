e1cc81ac3d1b0bf2c3ef8962adf9ba789b6c66c3
Sani Yusuf <saniyusuf@gmail.com>
2016-06-10 17:37:14 +0100

    added methods for editing expense and time log

--- www/js/services/editExpenseOrTimeLog.service.modal.js
+++ www/js/services/editExpenseOrTimeLog.service.modal.js
@@ -5,6 +5,70 @@
 
 (function () {
     'use strict';
-    
-    
+    angular
+        .module('starter.services')
+        .factory('EditExpenseOrTimeLogModal', EditExpenseOrTimeLogModal);
+
+    EditExpenseOrTimeLogModal.$inject = ['$ionicModal', '$rootScope', 'ProjectService', '$ionicLoading'];
+
+    function EditExpenseOrTimeLogModal($ionicModal, $rootScope, ProjectService, $ionicLoading) {
+        var $scope = $rootScope.$new(),
+            editExpenseOrTimeLogModalInstanceOptions = {
+                scope: $scope,
+                focusFirstInput: true
+            },
+            editExpenseModalTemplateUrl = RESOURCE_ROOT + 'templates/editTimeLogModal.html',
+            editTimeLogModalTemplateUrl = RESOURCE_ROOT + 'templates/editExpenseModal.html';
+
+        var editExpenseOrTimeLogModal = {
+            open: open
+        };
+
+        return editExpenseOrTimeLogModal;
+
+        function open(expenseOrTimeLog, expenseType) {
+            var templateUrl = '';
+
+            if(expenseType === 'time'){
+                $scope.expenseOrTimeLog = {
+                    Id: expenseOrTimeLog.Id,
+                    mobilecaddy1__Short_Description__c: expenseOrTimeLog.mobilecaddy1__Short_Description__c,
+                    mobilecaddy1__Duration_Minutes__c: expenseOrTimeLog.mobilecaddy1__Duration_Minutes__c
+                };
+                templateUrl = editTimeLogModalTemplateUrl;
+                
+            } else {
+                $scope.expenseOrTimeLog = {
+                    Id: expenseOrTimeLog.Id,
+                    mobilecaddy1__Short_Description__c: expenseOrTimeLog.mobilecaddy1__Short_Description__c,
+                    mobilecaddy1__Expense_Amount__c: expenseOrTimeLog.mobilecaddy1__Expense_Amount__c,
+                    mobilecaddy1__Expense_Image__c: expenseOrTimeLog.mobilecaddy1__Expense_Image__c
+                };
+                templateUrl = editExpenseModalTemplateUrl;
+            }
+
+            $ionicModal.fromTemplateUrl(
+                templateUrl,
+                editExpenseOrTimeLogModalInstanceOptions
+
+            ).then(function (modalInstance) {
+                $scope.close = function () {
+                    closeAndRemove(modalInstance);
+                };
+                return modalInstance.show();
+            });
+        }
+
+        function closeAndRemove(modalInstance) {
+            return modalInstance.hide()
+                .then(function () {
+                    return modalInstance.remove();
+                });
+        }
+
+        function updateExpenseOrTimeLog() {
+            ProjectService.updateExpenseOrTimeLog($scope.expenseOrTimeLog)
+        }
+    }
+
 })();
