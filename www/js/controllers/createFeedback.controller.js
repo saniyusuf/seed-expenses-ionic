0f017946f099a0df1337c86efbf14f6fb2b15a94
Sani Yusuf <saniyusuf@gmail.com>
2016-06-20 15:12:18 +0100

    inital commit

---
+++ www/js/controllers/createFeedback.controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Sani Yusuf on 20/06/2016.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('starter.controllers')
+        .controller('CreateFeedbackController', CreateFeedbackController);
+
+    CreateFeedbackController.$inject = [];
+    function CreateFeedbackController() {
+
+    }
+
+})();
