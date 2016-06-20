8857c8fafc5c0dddd65c9e1ff2849cae07338876
Sani Yusuf <saniyusuf@gmail.com>
2016-06-20 15:10:35 +0100

    created feedback service

--- www/js/services/feedback.service.js
+++ www/js/services/feedback.service.js
@@ -9,5 +9,26 @@
         .module('starter.services')
         .factory('FeedbackService', FeedbackService);
 
-    
+    FeedbackService.$inject = ['$q', 'devUtils', 'SyncService', 'FEEDBACK_TABLE_NAME'];
+
+    function FeedbackService($q, devUtils, SyncService, FEEDBACK_TABLE_NAME) {
+        var feedbackService = {
+            postFeedback: postFeedback
+        };
+        
+        return feedbackService;
+        
+        function postFeedback(feedBack) {
+            return devUtils.insertRecord(FEEDBACK_TABLE_NAME, feedBack)
+                .then(function (postFeedbackSuccessResponse) {
+                    SyncService.syncTables([FEEDBACK_TABLE_NAME], true);
+                    return $q.resolve(postFeedbackSuccessResponse);
+
+                }, function (postFeedbackFailureResponse) {
+                    return $q.reject(postFeedbackFailureResponse);
+                });
+        }
+
+    }
+
 })();
