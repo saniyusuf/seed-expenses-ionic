69a6ef21ff6127082e7714b3f4aa4bd79a2daf74
Sani Yusuf <saniyusuf@gmail.com>
2016-06-20 14:47:06 +0100

    initial commit

---
+++ www/js/services/feedback.service.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Sani Yusuf on 20/06/2016.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('starter.services')
+        .factory('FeedbackService', FeedbackService);
+
+    
+})();
