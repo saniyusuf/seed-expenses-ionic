90df4d7b38749a3c58bb608eeb99bc8f37d4d5fc
Sani Yusuf <saniyusuf@gmail.com>
2016-06-20 17:36:55 +0100

    implemented post feedback function

--- www/js/controllers/createFeedback.controller.js
+++ www/js/controllers/createFeedback.controller.js
@@ -9,9 +9,52 @@
         .module('starter.controllers')
         .controller('CreateFeedbackController', CreateFeedbackController);
 
-    CreateFeedbackController.$inject = [];
-    function CreateFeedbackController() {
+    CreateFeedbackController.$inject = ['FeedbackService', '$ionicLoading', '$ionicHistory'];
+    function CreateFeedbackController(FeedbackService, $ionicLoading, $ionicHistory) {
+        var vm = this;
 
+        vm.feedback = {
+            mobilecaddy1__Comment__c: '',
+            Name: 'TMP-' + Date.now()
+        };
+        vm.postFeedback = postFeedback;
+
+        function postFeedback() {
+            if(vm.feedback.comment === ''){
+                $ionicLoading.show({
+                    duration: 1200,
+                    noBackdrop: true,
+                    template: 'Please enter some text'
+                });
+
+                return false;
+            }
+
+            $ionicLoading.show({
+                template: 'Posting Your Feedback ..'
+            });
+            FeedbackService.postFeedback(vm.feedback)
+                .then(function () {
+                    vm.feedback = {
+                        mobilecaddy1__Comment__c: '',
+                        Name: 'TMP-' + Date.now()
+                    };
+                    $ionicHistory.goBack();
+                    $ionicLoading.show({
+                        noBackdrop: true,
+                        template: 'Thank You For Your Feedback',
+                        duration: 1200
+                    });
+                    
+                }, function () {
+                    $ionicLoading.show({
+                        noBackdrop: true,
+                        template: 'Feedback Not Posted. Please Try Again Shortly',
+                        duration: 1200
+                    });
+                });
+        }
+        
     }
 
 })();
