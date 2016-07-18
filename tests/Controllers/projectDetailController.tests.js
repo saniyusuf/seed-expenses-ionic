d3d1ed2dcf904486684cb636c7eb94b55cf4c5fe
Sani Yusuf <saniyusuf@gmail.com>
2016-07-18 12:20:55 +0100

    project details tests updated and passing

--- tests/Controllers/projectDetailController.tests.js
+++ tests/Controllers/projectDetailController.tests.js
@@ -3,31 +3,86 @@
  */
 
 
-describe('Project Detail Controller', function () {
+fdescribe('Project Detail Controller', function () {
+    var fullProjectDetailsStub,
+        createTimeAndExpenseModalStub,
+        editProjectDetailsModalStub,
+        controllerDependencies;
     beforeEach(module('starter.controllers'));
     beforeEach(function () {
-        var fullProjectDetailsStub = {
+        fullProjectDetailsStub = {
+            projectDetails: {
                 id: '1',
                 description: 'dummy test mock data'
-            };
-
-        it('Should Be Defined', inject(function ($controller) {
-            var projectDetail = $controller('ProjectDetailController', {
-                FullProjectDetails: fullProjectDetailsStub
-            });
-
-            expect(projectDetail).toBeDefined();
-        }));
-
-        it('Should Have An fullProjectDetails Property Equal To FullProjectDetails Stub', inject(function ($controller) {
-            var projectDetail = $controller('ProjectDetailController', {
-                FullProjectDetails: fullProjectDetailsStub
-            });
-
-            expect(projectDetail.fullProjectDetails).toEqual(fullProjectDetailsStub);
-        }));
+            }
+        };
+        createTimeAndExpenseModalStub = {
+            open: function (projectID, timeOrExpense) {}
+        };
+        editProjectDetailsModalStub = {
+            open: function (projectDetails) {}
+        };
+        controllerDependencies = {
+            FullProjectDetails: fullProjectDetailsStub,
+            CreateTimeAndExpenseModal: createTimeAndExpenseModalStub,
+            EditProjectDetailsModal: editProjectDetailsModalStub
+        };
 
     });
+
+    it('Should Be Defined', inject(function ($controller) {
+        var projectDetailController = $controller('ProjectDetailController', controllerDependencies);
+        expect(projectDetailController).toBeDefined();
+    }));
+
+    it('Should Have A fullProjectDetails Property Equal To FullProjectDetails Stub', inject(function ($controller) {
+        var projectDetailController = $controller('ProjectDetailController', controllerDependencies);
+        expect(projectDetailController.fullProjectDetails).toEqual(fullProjectDetailsStub);
+    }));
+
+    it('Should Create A New Expense Modal With Passed [projectID] & [expense] String', inject(function ($controller) {
+
+        controllerDependencies.$stateParams = {
+            projectID: '1234'
+        };
+        var projectDetailController = $controller('ProjectDetailController', controllerDependencies);
+
+        spyOn(createTimeAndExpenseModalStub, 'open');
+
+        projectDetailController.openCreateNewExpenseModal();
+
+        expect(createTimeAndExpenseModalStub.open)
+            .toHaveBeenCalledWith(controllerDependencies.$stateParams.projectID, 'expense');
+
+    }));
+
+    it('Should Create A New Time Modal With Passed [projectID] & [expense] String', inject(function ($controller) {
+
+        controllerDependencies.$stateParams = {
+            projectID: '1234'
+        };
+        var projectDetailController = $controller('ProjectDetailController', controllerDependencies);
+
+        spyOn(createTimeAndExpenseModalStub, 'open');
+
+        projectDetailController.openCreateNewTimeLogModal();
+
+        expect(createTimeAndExpenseModalStub.open)
+            .toHaveBeenCalledWith(controllerDependencies.$stateParams.projectID, 'time');
+
+    }));
+
+    it('Should Edit Project With A Cloned Copy From Resolved Data', inject(function ($controller) {
+        var projectDetailController = $controller('ProjectDetailController', controllerDependencies);
+
+        spyOn(editProjectDetailsModalStub, 'open');
+
+        projectDetailController.openEditProjectModal();
+
+        expect(editProjectDetailsModalStub.open)
+            .toHaveBeenCalledWith(fullProjectDetailsStub.projectDetails);
+
+    }));
 
 
 });
