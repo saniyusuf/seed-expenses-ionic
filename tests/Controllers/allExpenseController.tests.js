a3c3d6a036deeb37edd08e109ce49e5264b2e162
Sani Yusuf <saniyusuf@gmail.com>
2016-07-18 12:49:20 +0100

    fixed tests while added new test

--- tests/Controllers/allExpenseController.tests.js
+++ tests/Controllers/allExpenseController.tests.js
@@ -3,33 +3,46 @@
  */
 
 
-describe('All Expenses Controller Test', function () {
+fdescribe('All Expenses Controller Test', function () {
+    var allExpenseStub,
+        editExpenseOrTimeLogModalStub = {
+            open: function (expense, type) {}
+        },
+        controllerDependencies;
+    
     beforeEach(module('starter.controllers'));
     beforeEach(function () {
-        var allExpenseStub = [{
+        allExpenseStub = [{
             id: '1',
             description: 'dummy test mock data'
         }];
-
-        it('Should Be Defined', inject(function ($controller) {
-            var allExpenseController = $controller('AllExpensesController', {
-                AllExpenses: allExpenseStub
-            });
-
-            expect(allExpenseController).toBeDefined();
-
-        }));
-
-        it('Should Have An allExpenses Equal To AllExpenses Stub', inject(function ($controller) {
-            var allExpenseController = $controller('AllExpensesController', {
-                AllExpenses: allExpenseStub
-            });
-
-            expect(allExpenseController.allExpenses).toEqual(allExpenseStub);
-
-        }));
-
+        controllerDependencies = {
+            AllExpenses: allExpenseStub,
+            EditExpenseOrTimeLogModal: editExpenseOrTimeLogModalStub
+        };
     });
 
+    it('Should Be Defined', inject(function ($controller) {
+        var allExpenseController = $controller('AllExpensesController', controllerDependencies);
+        expect(allExpenseController).toBeDefined();
+    }));
+
+    it('Should Have An allExpenses Equal To AllExpenses Stub', inject(function ($controller) {
+        var allExpenseController = $controller('AllExpensesController', controllerDependencies);
+        expect(allExpenseController.allExpenses).toEqual(allExpenseStub);
+    }));
+
+    it('Should Edit Expense With Passed Expense', inject(function ($controller) {
+        var expenseStub = {
+            id: '1',
+            description: 'dummy test mock data'
+        };
+
+        var allExpenseController = $controller('AllExpensesController', controllerDependencies);
+        spyOn(editExpenseOrTimeLogModalStub, 'open');
+        allExpenseController.openEditExpenseModal(expenseStub);
+
+        expect(editExpenseOrTimeLogModalStub.open).toHaveBeenCalledWith(expenseStub, 'expense');
+    }));
 
 });
