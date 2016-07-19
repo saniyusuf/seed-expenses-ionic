3a5a2ffe148ffc42d5dadfd8680a8cd7daf630b2
Sani Yusuf <saniyusuf@gmail.com>
2016-07-19 11:50:23 +0100

    name change

---
+++ tests/Services/editExpenseOrTimeLogService.modal.tests.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sani Yusuf on 19/07/2016.
+ */
+
+window.RESOURCE_ROOT = '';
+var srojectServiceMock,
+    $ionicModalMock,
+    $ionicLoadingMock,
+    fromTemplateUrlParams,
+    $ionicModalInstanceStub;
+beforeEach(module('starter.services'));
